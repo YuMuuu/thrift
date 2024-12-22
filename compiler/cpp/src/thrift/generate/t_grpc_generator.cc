0192249fc382242b8c5fd4d23b6f8d1dbe45b1aa
yu miura <curelemonade15@gmail.com>
2024-12-22 12:48:51 +0900

    add name converter

--- compiler/cpp/src/thrift/generate/t_grpc_generator.cc
+++ compiler/cpp/src/thrift/generate/t_grpc_generator.cc
@@ -1,34 +1,29 @@
-#include <string>
+#include <filesystem>
 #include <fstream>
 #include <iostream>
-#include <vector>
-#include <map>
 #include <list>
-#include <filesystem>
+#include <map>
+#include <string>
 
-#include "thrift/platform.h"
 #include "thrift/generate/t_generator.h"
+#include "thrift/platform.h"
 
 using std::map;
 using std::ofstream;
 using std::ostringstream;
-using std::pair;
 using std::string;
-using std::stringstream;
-using std::vector;
 
 class t_grpc_generator : public t_generator {
- public:
-  t_grpc_generator(t_program* program, 
-                   const std::map<std::string, std::string> parsed_options, 
+public:
+  t_grpc_generator(t_program* program,
+                   const std::map<std::string, std::string> parsed_options,
                    const std::string& option_string)
-      : t_generator(program) {
+    : t_generator(program) {
     // (void)option_string;
     // std::map<std::string, std::string>::const_iterator iter;
 
-    //TODO: parsed_options,option_string でmergeや、service|message|enum でのfile分割、name space 単位でのfile分割、
-    //proto2などのバージョンサポートを行う
-    
+    // TODO: parsed_options,option_string でmergeや、service|message|enum でのfile分割、name space
+    // 単位でのfile分割、 proto2などのバージョンサポートを行う
 
     out_dir_base_ = "gen-grpc";
   }
@@ -36,7 +31,7 @@ class t_grpc_generator : public t_generator {
   void init_generator() override {
     MKDIR(get_out_dir().c_str());
 
-    std::string filename = get_out_dir() + program_->get_name() + ".proto";
+    std::string filename = get_out_dir() + to_lower_snake_case(program_->get_name()) + ".proto";
     f_proto_.open(filename.c_str());
 
     if (!f_proto_.is_open()) {
@@ -44,9 +39,7 @@ class t_grpc_generator : public t_generator {
     }
   }
 
-  void close_generator() override {
-    f_proto_.close();
-  }
+  void close_generator() override { f_proto_.close(); }
 
   void generate_program() override {
     generate_syntax();
@@ -63,82 +56,151 @@ class t_grpc_generator : public t_generator {
     }
   }
 
- private:
+private:
   std::ofstream f_proto_;
 
-  void generate_syntax() {
-    f_proto_ << "syntax = \"proto3\";\n\n";
-  }
+  void generate_syntax() { f_proto_ << "syntax = \"proto3\";\n\n"; }
 
-  void generate_package() {
-    f_proto_ << "package " << program_->get_namespace("grpc") << ";\n\n";
-  }
+  void generate_package() { f_proto_ << "package " << program_->get_namespace("grpc") << ";\n\n"; }
 
   void generate_struct(const t_struct* strct) {
-    f_proto_ << "message " << strct->get_name() << " {\n";
+    f_proto_ << "message " << to_pascal_case(strct->get_name()) << " {\n";
     int field_id = 1;
     for (const auto& field : strct->get_members()) {
-      f_proto_ << "  " << convert_type(field->get_type()) << " "
-               << field->get_name() << " = " << field_id++ << ";\n";
+      f_proto_ << "  "
+               << prefix(field) + convert_type(field->get_type()) + " "
+                      + to_lower_snake_case(field->get_name()) + " = " + std::to_string(field_id++)
+               << ";\n";
     }
     f_proto_ << "}\n\n";
   }
 
   void generate_enum(const t_enum* enm) {
-    f_proto_ << "enum " << enm->get_name() << " {\n";
+    f_proto_ << "enum " << to_pascal_case(enm->get_name()) << " {\n";
     for (const auto& value : enm->get_constants()) {
-      f_proto_ << "  " << value->get_name() << " = " << value->get_value() << ";\n";
+      // If the value is 0, add the suffix "UNSPECIFIED"
+      std::string enum_name = to_upper_snake_case(value->get_name());
+      if (value->get_value() == 0) {
+        enum_name += "_UNSPECIFIED";
+      }
+      f_proto_ << "  " << enum_name << " = " << value->get_value() << ";\n";
     }
     f_proto_ << "}\n\n";
   }
 
   void generate_service(const t_service* svc) {
-    f_proto_ << "service " << svc->get_name() << " {\n";
+    f_proto_ << "service " << to_pascal_case(svc->get_name()) << " {\n";
     for (const auto& func : svc->get_functions()) {
-      f_proto_ << "  rpc " << func->get_name() << " ("
-               << func->get_arglist()->get_name() << ") returns ("
-               << func->get_returntype()->get_name() << ");\n";
+      f_proto_ << "  rpc " << to_pascal_case(func->get_name()) << " ("
+               << to_pascal_case(func->get_arglist()->get_name()) << ") returns ("
+               << to_pascal_case(func->get_returntype()->get_name()) << ");\n";
     }
     f_proto_ << "}\n\n";
+  }
+
+  std::string to_pascal_case(const std::string& name) {
+    std::ostringstream result;
+    bool capitalize_next = true;
+
+    for (char c : name) {
+      if (std::isalnum(c)) {
+        if (capitalize_next) {
+          result << static_cast<char>(std::toupper(c));
+          capitalize_next = false;
+        } else {
+          result << static_cast<char>(std::tolower(c));
+        }
+      } else {
+        capitalize_next = true;
+      }
+    }
+    return result.str();
+  }
+
+  std::string to_lower_snake_case(const std::string& name) {
+    std::ostringstream result;
+
+    for (size_t i = 0; i < name.size(); ++i) {
+      char c = name[i];
+      if (std::isupper(c)) {
+        if (i > 0) {
+          result << "_";
+        }
+        result << static_cast<char>(std::tolower(c));
+      } else {
+        result << c;
+      }
+    }
+    return result.str();
+  }
+
+  std::string to_upper_snake_case(const std::string& name) {
+    std::ostringstream result;
+
+    for (size_t i = 0; i < name.size(); ++i) {
+      char c = name[i];
+      if (std::isupper(c) && i > 0) {
+        result << "_";
+      }
+      result << static_cast<char>(std::toupper(c));
+    }
+    return result.str();
+  }
+
+  std::string prefix(const t_field* field) {
+    switch (field->get_req()) {
+    case t_field::T_OPTIONAL:
+      return "optional ";
+    case t_field::T_REQUIRED:
+      return "";
+    default:
+      return "";
+      break;
+    }
   }
 
   std::string convert_type(const t_type* type) {
     if (type->is_base_type()) {
       const auto* base_type = static_cast<const t_base_type*>(type);
       switch (base_type->get_base()) {
-        case t_base_type::TYPE_VOID:
-          return "google.protobuf.Empty";
-        case t_base_type::TYPE_STRING:
-          return "string";
-        case t_base_type::TYPE_BOOL:
-          return "bool";
-        case t_base_type::TYPE_I8:
-        case t_base_type::TYPE_I16:
-        case t_base_type::TYPE_I32:
-          return "int32";
-        case t_base_type::TYPE_I64:
-          return "int64";
-        case t_base_type::TYPE_DOUBLE:
-          return "double";
-        default:
-          throw std::runtime_error("Unsupported base type");
+      case t_base_type::TYPE_VOID:
+        return "google.protobuf.Empty";
+      case t_base_type::TYPE_STRING:
+        return "string";
+      case t_base_type::TYPE_BOOL:
+        return "bool";
+      case t_base_type::TYPE_I8:
+      case t_base_type::TYPE_I16:
+      case t_base_type::TYPE_I32:
+        return "int32";
+      case t_base_type::TYPE_I64:
+        return "int64";
+      case t_base_type::TYPE_DOUBLE:
+        return "double";
+      default:
+        throw std::runtime_error("Unsupported base type");
       }
     } else if (type->is_list()) {
       const auto* list_type = static_cast<const t_list*>(type);
       return "repeated " + convert_type(list_type->get_elem_type());
-    } else if (type->is_map()|| type->is_set()) {
+    } else if (type->is_map() || type->is_set()) {
       t_type* k_type = ((t_map*)type)->get_key_type();
-      t_type* v_type = ((t_map*)type)->get_val_type();    
-      
-      return  "map<" + convert_type(k_type) + "," + convert_type(v_type) + ">";
-      //   throw std::runtime_error("Maps and sets are not supported directly in proto3"); 
-    } else if (type -> is_uuid()) {
+      t_type* v_type = ((t_map*)type)->get_val_type();
+
+      std::string k_type_name = k_type->get_name();
+      if (k_type_name != "string" && k_type_name != "int32" && k_type_name != "int64") {
+        throw std::runtime_error("Unsupported key type for Proto3 map: " + k_type_name);
+      }
+
+      return "map<" + convert_type(k_type) + "," + convert_type(v_type) + ">";
+      //   throw std::runtime_error("Maps and sets are not supported directly in proto3");
+    } else if (type->is_uuid()) {
       // Encording grpc string type. Reason for grpc is not support uuid type.
-        return "string";
-    } else if (type -> is_binary()) {
-        return "bytes";
+      return "string";
+    } else if (type->is_binary()) {
+      return "bytes";
     } else {
-      return type->get_name();
+      return to_pascal_case(type->get_name());
     }
   }
 };
